5b228bbfce019e3ce5fe34710354b3c7326e998f
Yury <31337@mail.ru>
2019-08-02 17:54:55 +0400

    Server implemented

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    private Thread serverThread;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private void onStartButtonClick(){
+        serverThread = new Thread(new Server());
+        serverThread.start();
+        System.out.println("Server started");
+
+    }
 }
---
+++ src/sample/Server.java
@@ -0,0 +1,36 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Server implements Runnable{
+
+    private static int PORT = 8000;
+    private static String LOCALHOST = "127.0.0.1";
+
+    @Override
+    public void run() {
+        try (
+            Socket socket = new Socket(LOCALHOST, PORT)
+        ) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String buffer;
+
+            while ((buffer = reader.readLine())!= null){
+
+                //TODO
+                System.out.println(buffer);
+            }
+
+        } catch (UnknownHostException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}

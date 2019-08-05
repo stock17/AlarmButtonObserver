9cef8446548fac11d702d68631c46aa9a6e03219
Yury <31337@mail.ru>
2019-08-05 18:55:21 +0400

    some fixes

--- src/main/java/com/yurima/alarmbuttonobserver/Main.java
+++ src/main/java/com/yurima/alarmbuttonobserver/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/main_form.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();

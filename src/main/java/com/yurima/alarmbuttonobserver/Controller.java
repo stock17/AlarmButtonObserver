30a631b8b1d15769e1588996ea10a3d98b3962e0
Yury <31337@mail.ru>
2019-08-12 15:45:15 +0400

    add controller for edit_form

--- src/main/java/com/yurima/alarmbuttonobserver/Controller.java
+++ src/main/java/com/yurima/alarmbuttonobserver/Controller.java
@@ -1,27 +1,36 @@
 package com.yurima.alarmbuttonobserver;
 
+import com.yurima.alarmbuttonobserver.edit.AddController;
 import com.yurima.alarmbuttonobserver.msg.AlarmMessage;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller implements Server.ServerStateListener {
 
     private Thread serverThread;
     private Server server;
-
     private Image connectedImage = new Image("/connected.png");
     private Image disconnectedImage = new Image("/disconnected.png");
 
     @FXML
     private Button startButton;
-
     @FXML
     private Label alarmMessageLabel;
+
 
     @FXML
     private void onStartButtonClick(){
@@ -29,6 +38,18 @@ public class Controller implements Server.ServerStateListener {
         server.setListener(this);
         serverThread = new Thread(server);
         serverThread.start();
+    }
+
+    @FXML
+    private void onAddButtonClick() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/edit_form.fxml"));
+        loader.setController(new AddController());
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 800, 600);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
     }
 
     @Override
---
+++ src/main/java/com/yurima/alarmbuttonobserver/edit/AddController.java
@@ -0,0 +1,18 @@
+package com.yurima.alarmbuttonobserver.edit;
+
+import javafx.fxml.FXML;
+
+public class AddController{
+
+    @FXML
+    private void onOkButtonClick(){
+        //TODO
+        System.out.println("OK");
+    }
+
+    @FXML
+    private void onCancelButtonClick(){
+        //TODO
+        System.out.println("Cancel");
+    }
+}

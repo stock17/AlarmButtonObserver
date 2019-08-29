878900deca7b3ca8dd60e0d1cbe2c433a7338b1c
Yury <31337@mail.ru>
2019-08-29 17:46:30 +0400

    fixed AlarmMessage class

--- src/main/java/com/yurima/alarmbuttonobserver/msg/AlarmMessageImpl.java
+++ src/main/java/com/yurima/alarmbuttonobserver/msg/AlarmMessageImpl.java
@@ -10,7 +10,7 @@ import java.util.Locale;
 
 public class AlarmMessageImpl implements AlarmMessage {
     //TODO
-    private int id;
+    private Integer id;
     private Date date;
     private AlarmMessage.Location location;
     private String phone;
@@ -21,15 +21,25 @@ public class AlarmMessageImpl implements AlarmMessage {
         this.id = id;
     }
 
+    //Create a new alarm message in a conductor
+    public AlarmMessageImpl(String phone) {
+        this.date = Calendar.getInstance().getTime();
+        this.phone = phone;
+    }
+
     //Recover alarm message from JSON object
     public AlarmMessageImpl(JSONObject json) throws JSONException {
-        this.id = json.getInt(ID_NAME);
+        if (json.has(ID_NAME))
+            this.id = json.getInt(ID_NAME);
+
         this.date = new Date(json.getLong(DATE_NAME));
+
         if (json.has(LONGITUDE_NAME) && json.has(LATITUDE_NAME)){
             double latitude = json.getDouble(LATITUDE_NAME);
             double longitude = json.getDouble(LONGITUDE_NAME);
             this.location = new Location(latitude, longitude);
         }
+
         if (json.has(PHONE_NAME)){
             this.phone = json.getString(PHONE_NAME);
         }
@@ -47,7 +57,8 @@ public class AlarmMessageImpl implements AlarmMessage {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder("test \n");
-        builder.append("id: ").append(id);
+        if (id != null)
+            builder.append("id: ").append(id);
         if (location != null){
             builder.append("\nLongitude: ").append(location.getLongitude())
                     .append("\nLatitude: ").append(location.getLatitude());
@@ -61,7 +72,8 @@ public class AlarmMessageImpl implements AlarmMessage {
     public JSONObject toJson() {
         JSONObject json = new JSONObject();
         try {
-            json.put(ID_NAME, id);
+            if (id != null)
+                json.put(ID_NAME, id);
             if (location != null) {
                 json.put(LONGITUDE_NAME,location.getLongitude());
                 json.put(LATITUDE_NAME,location.getLatitude());
